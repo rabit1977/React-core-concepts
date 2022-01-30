9b04d73e168a85ef0e7eb83c402504504c92dbb6
Rabit <rabit2245@gmail.com>
2022-01-31 00:02:32 +0100

    first comit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const greeting =  <h1>Hello React</h1>
+const rootNode = document.getElementById('root')
+
+ReactDOM.render(greeting, rootNode);
+
