fd91ace1e14d3eaf3b755358ccd0544a7d15db5a
Rabit <rabit2245@gmail.com>
2022-01-31 18:14:47 +0100

    second commit exercising react coding skills

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const greeting =  <h1>Hello React</h1>
-const rootNode = document.getElementById('root')
+function App() {
+  const people = ['Jane', 'John', 'Sime'];
+  function handleInputChange(event) {
+    const inputValue = event.target.value;
+    console.log(inputValue);
+  }
+  return (
+    <ul>
+      {people.map((person, i) => (
+        <Person key={i} person={person} />
+      ))}
+      <input type='text' onChange={handleInputChange} />
+    </ul>
+  );
+}
+function Person({ person }) {
+  function handlePersonClick(event) {
+    alert(person);
+    console.log(event);
+  }
+  return <li onClick={handlePersonClick}>{person}</li>;
+}
 
-ReactDOM.render(greeting, rootNode);
-
+const rootNode = document.getElementById('root');
+ReactDOM.render(<App />, rootNode);
