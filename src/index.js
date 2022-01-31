60b688fc1b88e0bb3f5972ed3b2dae792e9e2f60
Rabit <rabit2245@gmail.com>
2022-02-01 00:00:36 +0100

    added async fetch functionality to github API

--- src/index.js
+++ src/index.js
@@ -1,28 +1,65 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const endpoint = 'https://api.github.com/users/rabit1977';
+const baseUrl = 'https://api.github.com/users/';
 
 function App() {
+  const [username, setUsername] = useState('');
   const [user, setUser] = useState(null);
+  const searchInput = useRef();
 
   useEffect(() => {
-    fetch(endpoint)
-      .then((response) => response.json())
-      .then((data) => setUser(data));
-  }, []);
+    getUser();
 
-  return user ? (
-    <>
-      <div>{user.name}</div>
-      <div>{user.location}</div>
-      <div>{user.bio}</div>
-      <div>{user.followers}</div>
-      <div>{user.follow}</div>
-      <img src={user.avatar_url} style={{ height: 50 }} alt='' />
-    </>
-  ) : (
-    <p>...Loading</p>
+    // fetch(endpoint)
+    //   .then((response) => response.json())
+    //   .then((data) => setUser(data));
+  }, []);
+  function handleClearInput() {
+    searchInput.current.value = '';
+    searchInput.current.focus();
+  }
+
+  async function getUser() {
+    const response = await fetch(`${baseUrl}${username}`);
+    const data = await response.json();
+    setUser(data);
+  }
+
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='input username'
+        onChange={(event) => setUsername(event.target.value)}
+        ref={searchInput}
+      />
+      <button onClick={getUser}>Search</button>
+      <button onClick={handleClearInput}>Clear</button> <br />
+      <br />
+      {user ? (
+        <div>
+          <div>
+            <b>{user.name}</b>
+          </div>
+          <div>
+            <b>{user.location}</b>
+          </div>
+          <div>
+            <b>User bio:</b> {user.bio}
+          </div>
+          <div>
+            <b>followers:</b> {user.followers}
+          </div>
+          <div>
+            <b>following:</b> {user.following}
+          </div>
+          <img src={user.avatar_url} style={{ height: 50 }} alt='' />
+        </div>
+      ) : (
+        <p>...Loading</p>
+      )}
+    </div>
   );
 }
 
