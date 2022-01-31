dec4141f804fb05cdbdeb0ee5a0a56e32c0d3420
Rabit <rabit2245@gmail.com>
2022-01-31 20:50:17 +0100

    added onclick, onchange and other state functionality

--- src/index.js
+++ src/index.js
@@ -2,21 +2,42 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  const [language, setLanguage] = useState('javaScript');
-  const [yearsExperience, setYearsExperience] = useState(0);
+  const [developer, setDeveloper] = useState({
+    language: 'React',
+    yearsExperience: 0,
+    isEmployed: false,
+  });
+
+  //   const [language, setLanguage] = useState('javaScript');
+  //   const [yearsExperience, setYearsExperience] = useState(0);
+  function handleChangeLanguage(event) {
+    setDeveloper({
+      language: 'javaScript',
+      yearsExperience: 0,
+    });
+  }
+  function handleChangeYearsExperience(event) {
+    setDeveloper({ ...developer, yearsExperience: event.target.value });
+  }
+  function handleToggleEmployment() {
+    setDeveloper((prevState) => ({
+      ...developer,
+      isEmployed: !prevState.isEmployed,
+    }));
+  }
 
   return (
     <div>
-      <button onClick={() => setLanguage('python')}>Change Language</button>
-
+      <button onClick={handleToggleEmployment}>Toggle change employment</button>
+      <button onClick={handleChangeLanguage}>Change Language</button>
       <div>
-        <input
-          type='number'
-          onChange={(event) => setYearsExperience(event.target.value)}
-        />
+        <input type='number' onChange={handleChangeYearsExperience} />
       </div>
-      <p>I am learning {language}</p>
-      <p>I have {yearsExperience} years of experience</p>
+      <p>I am learning {developer.language}</p>
+      <p>I have {developer.yearsExperience} years of experience</p>
+      <p>
+        Employment status: {developer.isEmployed ? 'Employed' : 'Unemployed'}
+      </p>
     </div>
   );
 }
