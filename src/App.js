a8afcd5781ba464f5c3582cc53ee80ccfdcb5455
Rabit <rabit2245@gmail.com>
2022-02-02 21:55:45 +0100

    added useReducer example functionality

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import React, { createContext, useCallback, useEffect, useState } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react';
 import Header from './components/Header';
 import Login from './components/Login';
 import CreatePost from './components/CreatePost';
 import PostList from './components/PostList';
+import postReducer from './reducer';
 
 export const UserContext = createContext();
+export const PostContext = createContext({
+  posts: [],
+});
 
 function App() {
+  const intialPostState = useContext(PostContext);
+  const [state, dispatch] = useReducer(postReducer, intialPostState);
   const [user, setUser] = useState('ebibi');
-  const [posts, setPosts] = useState([]);
-  const [count, setCount] = useState(0);
+  // const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     document.title = user ? `${user}'s Feed` : 'Please login';
   }, [user]);
 
-  const handleAddPost = useCallback(
-    (newPost) => {
-      setPosts([newPost, ...posts]);
-    },
-    [posts]
-  );
+  // const handleAddPost = useCallback(
+  //   (newPost) => {
+  //     setPosts([newPost, ...state.posts]);
+  //   },
+  //   [posts]
+  // );
 
   if (!user) {
     return <Login setUser={setUser} />;
   }
 
   return (
-    <UserContext.Provider value={user}>
-      <Header user={user} setUser={setUser} />
-      <CreatePost user={user} handleAddPost={handleAddPost} />
-      <PostList posts={posts}  />
-    </UserContext.Provider>
+    <PostContext.Provider value={{ state, dispatch }}>
+      <UserContext.Provider value={user}>
+        <Header user={user} setUser={setUser} />
+        <CreatePost user={user} />
+        <PostList posts={state.posts} />
+      </UserContext.Provider>
+    </PostContext.Provider>
   );
 }
 
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,16 +1,17 @@
-import React, { useRef, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
+import { PostContext } from '../App';
 
-function CreatePost({ user, handleAddPost }) {
+function CreatePost({ user }) {
+  const { dispatch } = useContext(PostContext);
   const [content, setContent] = useState('');
   const [image, setImage] = useState(null);
   const imageInputRef = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    const post = { content, image, user };
-    handleAddPost(post);
-    // const newPosts = [post, ...posts];
-    // setPosts(newPosts);
+    const post = { content, image, user, id: Date.now() };
+    // handleAddPost(post);
+    dispatch({ type: 'ADD_POST', payload: { post } });
     setContent('');
     imageInputRef.current.value = '';
   }
